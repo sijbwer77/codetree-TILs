855a3cc45414b7075ecf5c50d10e4c1dfa133794
sijbwer77 <143161174+sijbwer77@users.noreply.github.com>
2024-03-18 15:03:19 +0900

    Title: 등급 매기기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240318/등급 매기기/ranking.py
@@ -0,0 +1,15 @@
+a = int(input())
+
+if a>=80:
+    if a>=90:
+        print('A')
+    else:
+        print('B')
+else:
+    if a>=70:
+        print('C')
+    else:
+        if a>=60:
+            print('D')
+        else:
+            print('F')
