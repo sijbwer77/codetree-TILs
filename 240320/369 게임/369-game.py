5bf819975311fe3d7332292822e97bdf5271e230
sijbwer77 <143161174+sijbwer77@users.noreply.github.com>
2024-03-20 14:43:44 +0900

    Title: 369 게임, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240320/369 게임/369-game.py
+++ 240320/369 게임/369-game.py
@@ -2,7 +2,7 @@ n = int(input())
 for i in range(1,n+1):
     if i%3==0:
         print(0,end=' ')
-    elif str(i).count('3')>=1:
+    elif str(i).count('3')>=1 or str(i).count('6')>=1 or str(i).count('9')>=1:
         print(0,end=' ')
     else:
         print(i,end=' ')
