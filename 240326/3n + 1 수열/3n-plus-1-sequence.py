a726db363c2744e3b127a62865cfec10873b23ba
sijbwer77 <143161174+sijbwer77@users.noreply.github.com>
2024-03-26 16:54:41 +0900

    Title: 3n + 1 수열, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

--- 240326/3n + 1 수열/3n-plus-1-sequence.py
+++ 240326/3n + 1 수열/3n-plus-1-sequence.py
@@ -1,11 +1,12 @@
 n = int(input())
 g = 0
 while True:
+    if n==1:
+        break
     if n%2==0:
         n = n/2
     elif n%2==1:
         n=n*3+1
     g+=1
-    if n==1:
-        break
+    
 print(g)
