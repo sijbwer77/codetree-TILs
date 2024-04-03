468e0a242876e5843464b294d2de9b73ed743416
sijbwer77 <143161174+sijbwer77@users.noreply.github.com>
2024-04-03 12:09:23 +0900

    Title: 별표 출력하기 12, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240403/별표 출력하기 12/print-star-12.py
+++ 240403/별표 출력하기 12/print-star-12.py
@@ -1,11 +1,12 @@
 n = int(input())
+
 for i in range(1,n+1):
     if i==1:
         print('* '*n)
     else:
-        if i%2==1:
-            print('    '*int((i-1)/2),end='')
-            print('   *'*(int(n/2)-int((i-1)/2)))
+        if i%2==0:
+            print('    '*int((i-2)/2))
+            print('   *'*int((n/2)+1-i))
         else:
-            print('    '*int((i-2)/2),end='')
-            print('   *'*(int(n/2)-int((i-2)/2)))
+            print('    '*int((i-1)/2))
+            print('   *'*int((n/1)+1-i))
