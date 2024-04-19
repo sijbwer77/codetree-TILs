6b0b46420df272b10cb048d6c04503a79625eaf3
sijbwer77 <143161174+sijbwer77@users.noreply.github.com>
2024-04-19 19:12:48 +0900

    Title: 최대공약수 구하기, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240419/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240419/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -3,6 +3,7 @@ m,n = map(int,input().split())
 def eo(m,n):
     for i in range(m+n+1,1,-1):
         if m%i==0 and n%i==0:
-            return(i)
+            return i
             break
+    return 1
 print(eo(m,n))
